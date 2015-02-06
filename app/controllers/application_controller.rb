f85aeff143415c6f87d525a55e160663440214da
Yury Kotov <bairkan@gmail.com>
2015-02-06 14:43:34 +0400

    Improvemets of OPTIONS request handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  after_action :set_access_control_headers
+
+  def handle_options_request
+    head(:no_content) if request.request_method == 'OPTIONS'
+  end
+
+  private
+
+  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS?redirectlocale=en-US&redirectslug=HTTP_access_control
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, X-CSRF-Token'
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['X-Frame-Options'] = 'GOFORIT'
+  end
 end
